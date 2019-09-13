d6a935e946d4768a4efe57560ab0057de094e851
Pramit Mallick <prmallic@microsoft.com>
2019-09-13 17:43:40 -0400

    first commit

---
+++ HapticSampler/HapticView.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace HapticSampler
+{
+    public partial class HapticView : UIView
+    {
+        public HapticView (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
